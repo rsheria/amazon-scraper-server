061096eabe74e6cb760b828b44bf99367139ec36
rsheria <rsheria2009@gmail.com>
2025-04-11 17:29:05 +0200

    Add Docker support for Render deployment

--- amazon-scraper.js
+++ amazon-scraper.js
@@ -107,12 +107,19 @@ async function fetchBookDataFromAmazon(url) {
       args: [
         '--no-sandbox',
         '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--disable-accelerated-2d-canvas',
+        '--no-first-run',
+        '--no-zygote',
+        '--single-process',
+        '--disable-gpu',
         '--disable-infobars',
         '--window-position=0,0',
         '--ignore-certificate-errors',
         '--ignore-certificate-errors-spki-list',
         '--user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36"'
-      ]
+      ],
+      executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || undefined
     });
 
     try {
